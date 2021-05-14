634946c78ebc6a9e5ee7f1dc267f487a6e765708
mathew tan <mathew12tan@gmail.com>
2021-05-14 10:21:15 +0800

    second commit

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const qnas = document.querySelectorAll(".qna");
 
 qnas.forEach((element) => {
+  const qnaq = element.querySelector('.qna-q');
   const question = element.querySelector(".question");
   const arrow = element.querySelector(".arrow");
   const answer = element.querySelector(".answer");
 
-  element.addEventListener("click", function () {
-    question.classList.toggle('questionFont');
-    arrow.classList.toggle('arrowAngle');
-    answer.classList.toggle('answerToggle');
+  qnaq.addEventListener("click", function () {
+    question.classList.toggle("questionFont");
+    arrow.classList.toggle("arrowAngle");
+    answer.classList.toggle("answerToggle");
   });
 });
+
 
